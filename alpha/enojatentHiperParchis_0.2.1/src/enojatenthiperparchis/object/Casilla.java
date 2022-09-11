a961de08bea2663ae41a3c72c3f27bb9a3632e11
martin-sainos-demian <111402496+martin-sainos-demian@users.noreply.github.com>
2022-09-11 16:31:21 -0500

    Add files via upload

---
+++ alpha/enojatentHiperParchis_0.2.1/src/enojatenthiperparchis/object/Casilla.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enojatenthiperparchis.object;
+
+import enojatenthiperparchis.All;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author PC
+ */
+public class Casilla extends Object {
+
+    public Canica canica;
+    public boolean hueco=false;
+    
+    public Casilla(All all, float x, float y, int id, Table table){
+        super(all, x, y, id, table.getState());
+    }
+    public Casilla(All all, float x, float y, int id, Table table, boolean hueco){
+        this(all,x,y,id,table);
+        this.hueco=hueco;
+    }
+    
+    @Override
+    public void tick() {
+        super.updateHitbox();
+        if(!hueco){
+            canica=null;
+        }
+    }
+
+    @Override
+    public void render(Graphics g){
+        if(hueco){
+            g.drawImage(all.sprites().huecoBack[0],(int)x,(int)y,null);
+            if(canica!=null){
+                canica.render(g, true);
+            }
+            g.drawImage(all.sprites().huecoFront[0],(int)x,(int)y,null);    
+        }else{
+            g.drawImage(all.sprites().sinHueco,(int)x,(int)y,null);
+        }
+        
+    }
+    public void render(Graphics g, BufferedImage front, BufferedImage back) {
+        if(hueco){
+            g.drawImage(back,(int)x,(int)y,null);
+            if(canica!=null){
+                canica.render(g, true);
+            }
+            g.drawImage(front,(int)x,(int)y,null);    
+        }else{
+            g.drawImage(all.sprites().sinHueco,(int)x,(int)y,null);
+        }
+    }
+    
+    public void setCanica(Canica canica){
+        if(hueco){
+            this.canica=canica;
+            canica.assigned=true;
+        }
+        if(canica==null){
+            this.canica.assigned=false;
+        }
+    }
+}
---
+++ alpha/enojatentHiperParchis_0.2.1/src/enojatenthiperparchis/object/Object.java
@@ -0,0 +1,48 @@
+
+package enojatenthiperparchis.object;
+
+import enojatenthiperparchis.All;
+import enojatenthiperparchis.states.State;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public abstract class Object {
+    public float x, y;
+    public int id;
+    State state;
+    All all;
+    public Rectangle hitbox;
+    
+    public Object(All all, float x, float y, int id, State state, int width, int height){
+        this.x=x;
+        this.y=y;
+        this.id=id;
+        this.state=state;
+        this.all=all;
+        hitbox=new Rectangle((int)x, (int)y, width,  height);
+    }
+    
+    public Object(All all, float x, float y, int id, State state){
+        this(all, x, y, id, state, all.getDefaultWidth(), all.getDefaultHeight());
+    }
+    
+    public void updateHitbox(){
+        hitbox.x=(int)x;
+        hitbox.y=(int)y;
+    }
+    
+    public abstract void tick();
+    public abstract void render(Graphics g);
+
+    public boolean isOverlapping(Object other) {
+    if (hitbox.y >= other.hitbox.y+other.hitbox.height
+      || hitbox.y+hitbox.height <= other.hitbox.y) {
+        return false;
+    }
+    if (hitbox.x >= other.hitbox.x+other.hitbox.width
+      || hitbox.x+hitbox.width <= other.hitbox.x) {
+        return false;
+    }
+    return true;
+}
+}
