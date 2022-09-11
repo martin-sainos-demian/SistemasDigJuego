8e93c5f5c3db4a3a3d8a3005e61797a8449de0c5
martin-sainos-demian <111402496+martin-sainos-demian@users.noreply.github.com>
2022-09-11 16:48:54 -0500

    Add files via upload

---
+++ enojatentHiperParchis_1.0/src/enojatenthiperparchis/gfx/Camara.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enojatenthiperparchis.gfx;
+
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author PC
+ */
+public class Camara {
+    public BufferedImage rotate(int angle, BufferedImage image){
+        double rotationRequired = Math.toRadians (angle);
+        double locationX = image.getWidth() / 2;
+        double locationY = image.getHeight() / 2;
+        AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
+        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+
+        // Drawing the rotated image at the required drawing locations
+        return op.filter(image, null);
+    }
+}
